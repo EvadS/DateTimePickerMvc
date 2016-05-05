2496658bc8104e23e1b28e7f09112f3610a732c3
Skiba Evgeniy <Skiba.Evgeniy@gmail.com>
2016-05-05 15:02:58 +0300

    add model and EF context

---
+++ DateTimePickerMvc/Models/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DateTimePickerMvc.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Имя")]
+        public string Name { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd'/'MM'/'yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Дата рождения")]
+        public DateTime BirthDate { get; set; }
+
+        [Display(Name = "Страна")]
+        public string State { get; set; }
+    }
+}
