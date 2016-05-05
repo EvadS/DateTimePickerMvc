6299932b6a6caade1615a96ee4ec6e8b04936d67
Skiba Evgeniy <Skiba.Evgeniy@gmail.com>
2016-05-05 15:05:10 +0300

    add personController (index and create action); changed default route setting

---
+++ DateTimePickerMvc/Controllers/PersonsController.cs
@@ -0,0 +1,37 @@
+﻿using DateTimePickerMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DateTimePickerMvc.Controllers
+{    
+    public class PersonsController : Controller
+    {
+        private PersonContext db = new PersonContext();
+
+        public ActionResult Index()
+        {
+            return View(db.Persons);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Persons.Add(person);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(person);
+        }
+    }
+}
