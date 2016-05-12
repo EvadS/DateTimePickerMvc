3bb75ee8c3800943fc9e69fdecddacaf87383e17
Skiba Evgeniy <Skiba.Evgeniy@gmail.com>
2016-05-12 16:39:21 +0300

    обновлено jquery/ доработал бандлы (валидация/ datetime / ui). вынесены скрыпты в отдельные файлы. Юзаем бандлы

--- DateTimePickerMvc/App_Start/BundleConfig.cs
+++ DateTimePickerMvc/App_Start/BundleConfig.cs
@@ -14,22 +14,26 @@ namespace DateTimePickerMvc.App_Start
                        "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js",
-                         "~/Scripts/helpdesk.js"));
+                        "~/Scripts/jquery-ui-{version}.js"));
+
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                        "~/Content/themes/base/jquery.ui.core.css",
-                        "~/Content/themes/base/jquery.ui.resizable.css",
-                        "~/Content/themes/base/jquery.ui.selectable.css",
-                        "~/Content/themes/base/jquery.ui.accordion.css",
-                        "~/Content/themes/base/jquery.ui.autocomplete.css",
-                        "~/Content/themes/base/jquery.ui.button.css",
-                        "~/Content/themes/base/jquery.ui.dialog.css",
-                        "~/Content/themes/base/jquery.ui.slider.css",
-                        "~/Content/themes/base/jquery.ui.tabs.css",
-                        "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
+                "~/Content/themes/smoothness/jquery-ui.smoothness.css"));
+
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+            "~/Content/themes/base/jquery.ui.core.css",
+            "~/Content/themes/base/jquery.ui.autocomplete.css",
+            "~/Content/themes/base/jquery.ui.datepicker.css",
+            "~/Content/themes/base/jquery.ui.theme.css"));
+
+            // Add your jQueryUI styles
+            bundles.Add(new StyleBundle("~/Content/themes/smoothness/css").Include(
+                            "~/Content/themes/smoothness/jquery-ui.smoothness.css"));
+
+
+            bundles.Add(new ScriptBundle("~/jquery.validate").Include(
+                 "~/Scripts/jquery.validate.js",
+                 "~/Scripts/jquery.validate.unobtrusive.js"));
         }
     }
 }
