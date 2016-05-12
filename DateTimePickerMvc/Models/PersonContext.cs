96cdf74c18d572101894ff8b21aff775ea7ff6e1
Skiba Evgeniy <Skiba.Evgeniy@gmail.com>
2016-05-12 11:39:20 +0300

    указываем строку подключения к базе данны�

--- DateTimePickerMvc/Models/PersonContext.cs
+++ DateTimePickerMvc/Models/PersonContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
@@ -9,5 +10,14 @@ namespace DateTimePickerMvc.Models
     public class PersonContext : DbContext
     {
         public DbSet<Person> Persons { get; set; }
+
+        public PersonContext()
+            : base()
+        {
+            var cs = ConfigurationManager.ConnectionStrings["ConnName"]
+                                         .ConnectionString;
+
+            this.Database.Connection.ConnectionString = cs;
+        }
     }
 }
